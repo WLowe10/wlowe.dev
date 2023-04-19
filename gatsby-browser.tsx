776a59539598ff7a7db31942c2d722e644c2bcac
Wesley Lowe <wesley.lowe@yahoo.com>
2023-04-19 18:52:48 -0500

    added fonts again

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Root } from "./src/root";
 import { PageLayout } from "./src/components/layouts";
 
-// import "reset-css";
-// import "@global/assets/fonts/saiyan-sans/font.css";
-// import "@fontsource/nunito-sans/400.css";
-// import "@fontsource/nunito-sans/600.css";
-// import "@fontsource/nunito-sans/700.css";
-// import "@fontsource/inter/400.css";
-// import "@fontsource/ubuntu/400.css";
-// import "@fontsource/satisfy/400.css";
+import "reset-css";
+import "@global/assets/fonts/saiyan-sans/font.css";
+import "@fontsource/nunito-sans/400.css";
+import "@fontsource/nunito-sans/600.css";
+import "@fontsource/nunito-sans/700.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/ubuntu/400.css";
+import "@fontsource/satisfy/400.css";
 
 export const wrapRootElement = ({ element, props }) => {
     return (
